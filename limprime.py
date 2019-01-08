b1f6b9a7181bb9532faed1485f4cad496a843fe9
ezhilarasi-g <46440782+ezhilarasi-g@users.noreply.github.com>
2019-01-08 18:39:52 +0530

    Create limprime.py

---
+++ limprime.py
@@ -0,0 +1,19 @@
+a,b=map(int,input().split())
+x=' '
+for num in range(a+1,b):
+	if num>1:
+		for i in range(2,num):
+			if num%i==0:
+				break
+		else:
+				x=x+str(num)+' '
+print(x.strip())a,b=map(int,input().split())
+x=' '
+for num in range(a+1,b):
+	if num>1:
+		for i in range(2,num):
+			if num%i==0:
+				break
+		else:
+				x=x+str(num)+' '
+print(x.strip())
