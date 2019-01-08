314bba1fa1e4a6d119935730f130e6ef89fd3cf4
ezhilarasi-g <46440782+ezhilarasi-g@users.noreply.github.com>
2019-01-08 18:42:07 +0530

    Update limprime.py

--- limprime.py
+++ limprime.py
@@ -7,13 +7,4 @@ for num in range(a+1,b):
 				break
 		else:
 				x=x+str(num)+' '
-print(x.strip())a,b=map(int,input().split())
-x=' '
-for num in range(a+1,b):
-	if num>1:
-		for i in range(2,num):
-			if num%i==0:
-				break
-		else:
-				x=x+str(num)+' '
 print(x.strip())
