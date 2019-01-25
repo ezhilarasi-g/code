470e8adae9519adbfbbf86e8252ae376fd07ed93
ezhilarasi-g <46440782+ezhilarasi-g@users.noreply.github.com>
2019-01-25 22:59:01 +0530

    Create aavowel.py

---
+++ aavowel.py
@@ -0,0 +1,12 @@
+3ezhil
+n=input()
+c=0
+lis=['A','E','I','O','U','a','e','i','o','u']
+for i in n:
+    if i in lis:
+        c=c+1
+        break
+if c!=0:
+    print("yes")
+else:
+    print("no")
