cc98b7389f5fe1e7d7b8b87d430296eab9ff2583
ezhilarasi-g <46440782+ezhilarasi-g@users.noreply.github.com>
2019-01-25 15:53:39 +0530

    Create numdigittonum.py

---
+++ numdigittonum.py
@@ -0,0 +1,7 @@
+#ezhil
+n=input()
+lis={"1":"One","2":"Two","3":"Three","4":"Four","5":"Five","6":"Six","7":"Seven","8":"Eight","9":"Nine","0":"Zero"}
+for i in range(len(lis)):
+    print(lis[n])
+    break
+    
