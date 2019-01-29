b349f9d4a624707e4a524edb36738fa8585e2cae
ezhilarasi-g <46440782+ezhilarasi-g@users.noreply.github.com>
2019-01-29 17:00:00 +0530

    Update revremovowel.py

--- revremovowel.py
+++ revremovowel.py
@@ -1,11 +1,10 @@
 #ezhil
 a=int(input())
 b=input()
-c=b.lower()
-s=c[::-1]
+s=b[::-1]
 l=list(str(s))
 for i in l:
-    if i=="a" or i=="e" or i=="i" or i=="o" or i=="u":
+    if i=="a" or i=="e" or i=="i" or i=="o" or i=="u" or i=="A" or i=="E" or i=="O" or i=="I" or i=="U":
         l.remove(i)
         d=''.join(map(str,l))
 print(d)
