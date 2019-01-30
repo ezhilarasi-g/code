b891b50def71a00468c9094164a9bfcdff6cca57
ezhilarasi-g <46440782+ezhilarasi-g@users.noreply.github.com>
2019-01-30 16:15:09 +0530

    Create anagram.py

---
+++ anagram.py
@@ -0,0 +1,17 @@
+#ezhil
+n=int(input())
+s="kabali"
+a=[]
+d=0
+for i in range(0,n):
+    a.append(input())
+for j in a:
+    c=0
+    for k in j:
+        if k in s:
+            if s.count(k)==j.count(k):
+                c=c+1
+    if c==len(s):
+        d=d+1
+print(d)
+        
