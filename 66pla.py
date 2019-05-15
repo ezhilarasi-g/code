31ce035ddda56cd445f12900f989b2a7dc52f8bf
ezhilarasi-g <46440782+ezhilarasi-g@users.noreply.github.com>
2019-05-15 15:44:07 +0530

    Update 66pla.py

--- 66pla.py
+++ 66pla.py
@@ -1,8 +1,9 @@
 #ezhil
-n,k=map(int,input().split())
+n=list(map(int,input().split()))
 lis=list(map(int,input().split()))
 t=[]
 b=[]
+k=n[len(n)-1]
 for i in lis:
     s=lis.count(i)
     if s==k:
