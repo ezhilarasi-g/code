deaf651d909bbe2dd8713349e35c41bd19e4dd97
ezhilarasi-g <46440782+ezhilarasi-g@users.noreply.github.com>
2019-05-17 09:25:15 +0530

    Create 7pro.py

---
+++ 7pro.py
@@ -0,0 +1,14 @@
+#ezhil
+n=int(input())
+a=list(map(int,input().split()))
+for i in range(0,n):
+    if i<n-1:
+        b=' '
+    else:
+        b=''
+    if i%2==0:
+        if a[i]%2!=0:
+            print(a[i],end=b)
+    elif i%2!=0:
+        if a[i]%2==0:
+            print(a[i],end=b)
