166cd1577920fa766d1df78b84cd1a7695107679
ezhilarasi-g <46440782+ezhilarasi-g@users.noreply.github.com>
2019-05-17 10:02:48 +0530

    Create 8pro.py

---
+++ 8pro.py
@@ -0,0 +1,12 @@
+#ezhil
+n=int(input())
+l=list(map(int,input().split()))
+l1=[]
+for i in range(0,len(l)):
+	for j in range(i+1,len(l)):
+		for k in range(j+1,len(l)):
+			if l[i]+l[j]==l[k]:
+				l1.append(l[k])
+				#result
+				print(l[i],l[j],l[k])
+				break
