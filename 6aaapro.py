a9d25d60024bc93b339d6151bedf534b31e79faf
ezhilarasi-g <46440782+ezhilarasi-g@users.noreply.github.com>
2019-05-17 09:10:02 +0530

    Rename 6pro.py to 6aaapro.py

---
+++ 6aaapro.py
@@ -0,0 +1,14 @@
+#ezhil
+n=int(input())
+a=list(map(int,input().split()))
+b=[]
+c=0
+for i in a:
+    if i not in b:
+        b.append(i)
+    else:
+        print(i)
+        c=c+1
+        break
+if c==0:
+    print("unique")
