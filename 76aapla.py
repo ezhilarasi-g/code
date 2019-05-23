39efdddaf7ae2844abf2a0cf7bf1639ac62d317d
ezhilarasi-g <46440782+ezhilarasi-g@users.noreply.github.com>
2019-05-23 20:54:28 +0530

    Create 76aapla.py

---
+++ 76aapla.py
@@ -0,0 +1,16 @@
+#ezhil
+n=int(input())
+l=list(map(int,input().split()))
+c=0
+s=0
+for i in range(0,len(l)):
+    if l[i]%2==0:
+        c+=1
+        a=l[i]
+    else:
+        s+=1
+        b=l[i]
+if c==1:
+    print(a)
+else:
+    print(b)
