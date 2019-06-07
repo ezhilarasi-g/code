450101e0588270c615c39ffedf35a3ae7cf3e732
ezhilarasi-g <46440782+ezhilarasi-g@users.noreply.github.com>
2019-06-07 20:09:26 +0530

    Update 68hunt.py

--- 68hunt.py
+++ 68hunt.py
@@ -1,5 +1,5 @@
 #ezhil
 n=int(input())
 lis=list(map(int,input().split()))
-print((lis.index(max(lis))+1),end=" ")
-print((lis.index(min(lis))+1))
+print((lis.index(min(lis))+1),end=" ")
+print((lis.index(max(lis))+1))
